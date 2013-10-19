77dfe38bd4668ed73fd6fb3f86a558982a9263fd
Stanislaw Zawadzki <st.zawadzki@gmail.com>
2013-10-19 11:12:41 +0200

    Fide id for players

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,13 +1,27 @@
 class Player < ActiveRecord::Base
 
-	attr_accessor :name, :surname, :middlename, :fide_id
+  attr_accessor :name, :surname, :middlename, :fide_id
+  NAME_REGEX = /\A[\s[:alpha:]-]*\z/u
 
-	NAME_REGEX = /\A[\s[:alpha:]-]*\z/u
+  validates :name, presence: true, format: { with: NAME_REGEX }, length: {minimum: 2, maximum: 32}
+  validates :middlename, format: { with: NAME_REGEX }, length: {minimum: 2, maximum: 32}, allow_nil: true
+  validates :surname, presence: true, format: { with: NAME_REGEX }, length: {minimum: 2, maximum: 32}
 
-	validates :name, presence: true, format: { with: NAME_REGEX }, length: {minimum: 2, maximum: 32}
-	validates :middlename, format: { with: NAME_REGEX }, length: {minimum: 2, maximum: 32}, allow_nil: true
-	validates :surname, presence: true, format: { with: NAME_REGEX }, length: {minimum: 2, maximum: 32}
+  before_save :set_fide_id, if: proc {|player| player.fide_id.nil?}
 
 
+private
+
+  def set_fide_id
+    File.open("#{Rails.root}/public/standard_oct13frl.txt").each_line do |line|
+      @fide_id = extract_id_from_line(line) if line.match(name.parameterize.camelcase) && line.match(surname.parameterize.camelcase)
+    end
+    self.fide_id = @fide_id
+  end
+
+  def extract_id_from_line(line)
+    line =~ / /
+    $`
+  end
 
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -3,44 +3,44 @@ require 'spec_helper'
 
 describe Player do
   context 'creating player' do
-	  it 'should create player with only name data' do
-	    FactoryGirl.build(:player).should be_valid
-	  end
+    it 'should create player with only name data' do
+      FactoryGirl.build(:player).should be_valid
+    end
 
-	  it 'should create player with extra data' do
-	    FactoryGirl.build(:player_with_date_of_birth).should be_valid
-	    FactoryGirl.build(:player_with_fide_id).should be_valid
-	  end
+    it 'should create player with extra data' do
+      FactoryGirl.build(:player_with_date_of_birth).should be_valid
+      FactoryGirl.build(:player_with_fide_id).should be_valid
+    end
 
-	  it 'should not create player without name' do
-	  	FactoryGirl.build(:player, name: nil).should_not be_valid
-	  end
+    it 'should not create player without name' do
+      FactoryGirl.build(:player, name: nil).should_not be_valid
+    end
 
-	  it 'should not create player without surname' do
-	    FactoryGirl.build(:player, surname: nil).should_not be_valid
-	  end
+    it 'should not create player without surname' do
+      FactoryGirl.build(:player, surname: nil).should_not be_valid
+    end
 
-	  it 'should not create player with too short surname' do
-	    FactoryGirl.build(:player, name: 'a').should_not be_valid
-	  end
+    it 'should not create player with too short surname' do
+      FactoryGirl.build(:player, name: 'a').should_not be_valid
+    end
 
-	  it 'should not create player with too long surname' do
-	    FactoryGirl.build(:player, name: 'a'*33).should_not be_valid
-	  end
+    it 'should not create player with too long surname' do
+      FactoryGirl.build(:player, name: 'a'*33).should_not be_valid
+    end
 
-	  it 'should not create player with too short surname' do
-	    FactoryGirl.build(:player, surname: 'a').should_not be_valid
-	  end
+    it 'should not create player with too short surname' do
+      FactoryGirl.build(:player, surname: 'a').should_not be_valid
+    end
 
-	  it 'should not create player with too long surname' do
-	    FactoryGirl.build(:player, surname: 'a'*33).should_not be_valid
-	  end
+    it 'should not create player with too long surname' do
+      FactoryGirl.build(:player, surname: 'a'*33).should_not be_valid
+    end
 
-	  it 'should create player without middlename' do
-	  	FactoryGirl.build(:player, middlename: nil).should be_valid
-	  end
+    it 'should create player without middlename' do
+      FactoryGirl.build(:player, middlename: nil).should be_valid
+    end
 
-	  it 'should not allow any chars besides from letters and hyphens and spaces' do
+    it 'should not allow any chars besides from letters and hyphens and spaces' do
       FactoryGirl.build(:player, name: 'a12345').should_not be_valid
       FactoryGirl.build(:player, name: 'abcdef').should be_valid
 
@@ -52,6 +52,15 @@ describe Player do
 
       FactoryGirl.build(:player, name: 'Beata', surname: 'Kądziołka-Zawadzka').should be_valid
       FactoryGirl.build(:player, name: 'José Enrique').should be_valid
-	  end	
+    end	
+
+    it 'should look for a player in FIDE database and assign fide_id to player' do 
+      player = FactoryGirl.create(:player, name: "Jolanta", surname: "Zawadzka")
+      player.fide_id.should eql "1122320"
+      
+      player = FactoryGirl.create(:player, name: "Beata", surname: "Kądziołka")
+      player.fide_id.should eql "1119990"
+    end
+
   end
 end 
