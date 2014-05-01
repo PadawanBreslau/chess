937318d66e542c4f29416a41a2e8d0fcede2927f
Stanislaw Zawadzki <st.zawadzki@gmail.com>
2014-05-01 23:47:27 +0200

    Tournament list

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -3,6 +3,11 @@ class TournamentsController < InheritedResources::Base
   actions :all, except: []
   FIELDS = [:tournament_name, :tournament_start, :tournament_finish, :round_number, :is_finished, :place, :url, :external_transmition_url, :event_id]
 
+  def index
+    @tournaments_grid = initialize_grid(Tournament)
+  end
+
+
   def permitted_params
     params.permit(tournament: FIELDS)
   end
